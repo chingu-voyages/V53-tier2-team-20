6a8181656b4c1556112296dee0508156c3b14b08
Chloe Zhou <chloez0607@gmail.com>
2025-02-14 09:19:07 +0800

    chore: fix lighthouse ci config & workflow

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,24 +1,10 @@
 module.exports = {
   ci: {
-    collect: {
-      staticDistDir: './dist',
-      numberOfRuns: 3,
-    },
     upload: {
       target: 'temporary-public-storage',
     },
     assert: {
       preset: 'lighthouse:recommended',
-      assertions: {
-        'csp-xss': 'off',
-        'uses-text-compression': 'off',
-        'non-composited-animations': 'off',
-        'unused-javascript': 'warn',
-        'render-blocking-resources': 'warn',
-        'total-blocking-time': ['warn', { maxNumericValue: 300 }],
-        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
-        'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
-      },
     },
   },
 };
