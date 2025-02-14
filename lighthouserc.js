013282bdcd441ff311833fc521040bf4ed8e93a5
Chloe Zhou <chloez0607@gmail.com>
2025-02-14 16:39:16 +0800

    chore: revert lighthouse assertions

--- lighthouserc.js
+++ lighthouserc.js
@@ -5,6 +5,16 @@ module.exports = {
     },
     assert: {
       preset: 'lighthouse:recommended',
+      assertions: {
+        'csp-xss': 'off',
+        'uses-text-compression': 'off',
+        'non-composited-animations': 'off',
+        'unused-javascript': 'warn',
+        'render-blocking-resources': 'warn',
+        'total-blocking-time': ['warn', { maxNumericValue: 300 }],
+        'first-contentful-paint': ['warn', { maxNumericValue: 2000 }],
+        'largest-contentful-paint': ['warn', { maxNumericValue: 2500 }],
+      },
     },
   },
 };
